2168828ca7fc7951ea76872c587963f714d47be8
Siddharth Tikekar <siddharth.tikekar@rtcamp.com>
2019-09-13 19:36:13 +0530

    Plugin structure and register wp-book cpt

---
+++ includes/class-wp-book-deactivator.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @since      1.0.0
+ *
+ * @package    Wp_Book
+ * @subpackage Wp_Book/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Book
+ * @subpackage Wp_Book/includes
+ * @author     Siddharth <siddharthantikekar@gmail.com>
+ */
+class Wp_Book_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
